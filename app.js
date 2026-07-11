602980e0f3253d3b515b1e9940a4257967afffdb
Bora Cahliyan <boracahliyan@gmail.com>
2026-07-11 19:43:33 +0300

    feat: add hourly weather list and dynamic city background

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { PEXELS_API_KEY } from './config.js';
+
 
 const button = document.getElementById("Enter_button");
 const city_input = document.getElementById("city_name");
@@ -34,7 +36,7 @@ async function getter() {
 
     console.log(lan,lon);
 
-    const apiurl_weather = `https://api.open-meteo.com/v1/forecast?latitude=${lan}&longitude=${lon}&&hourly=rain&current=temperature_2m&timezone=auto`;
+    const apiurl_weather = `https://api.open-meteo.com/v1/forecast?latitude=${lan}&longitude=${lon}&hourly=temperature_2m&current=temperature_2m&timezone=auto&forecast_days=1`;
 
 
     const raw_data_wet = await fetch(apiurl_weather);
@@ -42,11 +44,32 @@ async function getter() {
 
     console.log(data_wet);
 
-    document.getElementById("temperature").textContent = data_wet.current.temperature_2m; 
+    document.getElementById("temperature").textContent = data_wet.current.temperature_2m;
+    document.querySelector(".hourly_temp").innerHTML = "";
+
+    for (let hour = 0; hour <= 23; hour++) {
+        const table_data = document.createElement("td");
+        table_data.textContent = data_wet.hourly.temperature_2m[hour] + "°C";
+        document.querySelector(".hourly_temp").appendChild(table_data);
+        
+    }
+
+    document.querySelector(".table").style.display = 'block';
 
 
 
-    
+    const url = `https://pixabay.com/api/?key=${PEXELS_API_KEY}&q=${encodeURIComponent(city)}+city&image_type=photo&orientation=horizontal&pretty=true`;
+
+
+    const response = await fetch(url);
+    const city_photo = await response.json();    
+    const city_img = document.createElement("img");
+
+   if (city_photo.hits && city_photo.hits.length > 0) {
+    const imageUrl = city_photo.hits[0].largeImageURL;
+    document.body.style.backgroundImage = `url(${imageUrl})`;
+}
+
 
 }
 
@@ -66,7 +89,7 @@ function clicker(){
 const search_item = document.querySelectorAll("#autocomplete li");
 
 search_item.forEach(element => {
-      console.log("merhaba");
+
     element.addEventListener("mousedown", e => {
         document.querySelector("#city_name").value = element.textContent.trim();
     });
@@ -113,3 +136,5 @@ city_input.addEventListener("input", async e =>{
     clicker();
 
 });
+
+
